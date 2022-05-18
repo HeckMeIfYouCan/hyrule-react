0e048748e96482cbda451a791fba87969e2e67d5
Johannes Heckmann <johannes.heckmann@valtech.com>
2022-05-18 14:23:40 +0200

    minor adjustments

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return ( 
+    <div className="App">
+      <header className="App-header">
+        React-App
+      </header>
+
+      <div class="body">Test</div>
+    </div>
+  );
+}
+
+export default App;
